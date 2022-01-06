something wrong with denominator of gradient

aerostruct_paper/impinge_setup.py
@@ -4,7 +4,7 @@ import numpy as np
 #from tacs import TACS, elements, constitutive, functions
 
 outputDirectory = './results'
-aeroGridFile = f'imp_conv_2_145_145_25.cgns'
+aeroGridFile = f'imp_mphys_73_73_25.cgns'
 alpha = 0. #
 beta = 7.2833969362749187
 mach = 2.6381157549933598 #3.0
@@ -12,7 +12,7 @@ areaRef = 1.0
 chordRef = 1.0
 T = 254.02071103827234 #217.
 P = 4987.6905797938707 #2919.
-probName = 'impinge_test_4'
+probName = 'impinge_mphys'
 
 astar = [ 0.0,0.0]
 dist = []
@@ -114,14 +114,15 @@ aeroOptions = { #ADflow aero solver options
     }
 
 # Euler Bernoulli Structural Solver Options
+nelem = 30
 structOptions = {
     "name":probName,
-    "Nelem":60,
+    "Nelem":nelem,
     "L":0.254, #0.254, 
     "E":100000,
-    "force":np.ones(61)*1.0,
+    "force":np.ones(nelem+1)*1.0,
     "Iyy":None,
-    "th":np.ones(61)*0.001,
+    "th":np.ones(nelem+1)*0.001,
     "l_bound":2.0,
     "get_funcs":None
     }

aerostruct_paper/surrogate/pougrad.py
@@ -76,9 +76,15 @@ class POUSurrogate():
         g = self.g
 
         # exhaustive search for closest sample point, for regularization
+        dists = np.zeros(self.numsample)
         for i in range(self.numsample):
-            dist = np.sqrt(np.dot(x-xc[i],x-xc[i]) + self.delta)
-            mindist = min(mindist,dist)
+            dists[i] = np.sqrt(np.dot(x-xc[i],x-xc[i]) + self.delta)
+            
+        mindist = min(dists)
+        
+        # for i in range(self.numsample):
+        #     dist = np.sqrt(np.dot(x-xc[i],x-xc[i]) + self.delta)
+        #     mindist = min(mindist,dist)
 
         numer = 0
         denom = 0
@@ -91,7 +97,7 @@ class POUSurrogate():
             numer += local*expfac
             denom += expfac
 
-        return numer/denom
+        return 1/denom # numer/denom
 
     """
     Evaluate the gradient of the surrogate at the point x, with respect to x
@@ -102,9 +108,51 @@ class POUSurrogate():
     x : numpy array(dim)
         Query location
 
-    sgrad : numpy array(dim)
-        Gradient output
     """
-    def evalGrad(self, x, sgrad):
+    def evalGrad(self, x):
         sgrad = np.zeros(self.dim)
-        return
+
+        mindist = 1e100
+        xc = self.xc
+        f = self.f
+        g = self.g
+        imindist = 1e100
+        
+
+        # exhaustive search for closest sample point, for regularization
+        dists = np.zeros(self.numsample)
+        for i in range(self.numsample):
+            dists[i] = np.sqrt(np.dot(x-xc[i],x-xc[i]) + self.delta)
+            
+        mindist = min(dists)
+        imindist = np.argmin(dists)
+
+        dmindist = (1.0/mindist)*(x-xc[imindist])
+
+        numer = 0
+        denom = 0
+
+        dnumer = np.zeros(self.dim)
+        ddenom = np.zeros(self.dim)
+
+        sum = 0
+
+        for i in range(self.numsample):
+            dist = np.sqrt(np.dot(x-xc[i],x-xc[i]) + self.delta)
+            local = f[i] + np.dot(g[i], x-xc[i]) # locally linear approximation
+            expfac = np.exp(-self.rho*(dist-mindist))
+            numer += local*expfac
+            denom += expfac        
+
+            ddist = (1.0/np.sqrt(np.dot(x-xc[i],x-xc[i])+self.delta))*(x-xc[i])
+            dlocal = g[i]
+            dexp1 = -self.rho*expfac
+            dexp2 = self.rho*expfac
+
+            dnumer += expfac*dlocal + local*(dexp1*ddist + dexp2*dmindist)
+            ddenom += -(1./(expfac*expfac))*(dexp1*ddist + dexp2*dmindist)
+
+            sum += (dexp1*ddist + dexp2*dmindist)
+
+        xgrad = (1./denom)*dnumer + numer*ddenom
+        return ddenom # xgrad

aerostruct_paper/surrogate/test.py
@@ -19,14 +19,15 @@ def trueFunctionGrad(x):
     return g
 
 # generate "training" and "test" data
+dim = 2
 ntr = 10
 nte = 50
 
-tr = np.random.rand(ntr, 2)
+tr = np.random.rand(ntr, dim)
 fr = np.zeros(ntr)
-gr = np.zeros((ntr,2))
+gr = np.zeros((ntr,dim))
 
-te = np.random.rand(nte, 2)
+te = np.random.rand(nte, dim)
 fe = np.zeros(nte)
 
 for i in range(ntr):
@@ -45,4 +46,18 @@ for i in range(nte):
     fes[i] = pou.eval(te[i])
 
 err = fes - fe
+
+# check x gradient
+
+step = 1e-6
+
+festep = np.zeros(dim)
+for k in range(dim):
+    stepvec = np.zeros(dim)
+    stepvec[k] = step
+    festep[k] = pou.eval(te[0] + stepvec)
+
+xgradfd = (festep - fes[0])/step
+xgradad = pou.evalGrad(te[0])
+
 import pdb; pdb.set_trace()