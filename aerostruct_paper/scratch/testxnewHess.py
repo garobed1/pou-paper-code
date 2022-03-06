fixed directionality, fixed ignoring gradient, results seem nice so far

aerostruct_paper/scratch/testxnewHess.py
@@ -18,11 +18,11 @@ from smt.sampling_methods import LHS
 dim = 2
 rho = 100
 
-trueFunc = MultiDimJump(ndim=dim)#, theta=np.pi/4)
+trueFunc = MultiDimJump(ndim=dim, alpha = 15.)#, theta=np.pi/4)
 xlimits = trueFunc.xlimits
 sampling = LHS(xlimits=xlimits)
 
-nt0  = 10
+nt0  = 30
 dist = 0.01
 ntr = 10
 nte = 20
@@ -67,7 +67,7 @@ if(isinstance(gek, gekpls.GEKPLS) or isinstance(gek, POUSurrogate)):
         gek.set_training_derivatives(t0, g0[:,i:i+1], i)
 gek.train()
 
-criteria = HessianFit(gek, improve=0., neval=6, hessian="neighborhood", interp="honly", criteria="variance") #looCV(gek, approx=False)
+criteria = HessianFit(gek, g0, improve=ntr, neval=6, hessian="neighborhood", interp="honly", criteria="variance") #looCV(gek, approx=False)
 
 #Contour
 plt.figure(2)
@@ -166,18 +166,22 @@ options["localswitch"] = True
 #import pdb; pdb.set_trace()
 #gek.name = "GEKPLS"
 
-gek, criteria = adaptivesampling(trueFunc, gek, criteria, xlimits, ntr, options=options)
+gek, criteria, hist = adaptivesampling(trueFunc, gek, criteria, xlimits, ntr, options=options)
 
 t0 = gek.training_points[None][0][0]
 f0 = gek.training_points[None][0][1]
-b0 = criteria.bads
-
+# b0 = hist[0].bads
+# b1ind = hist[0].bad_nbhd
+# b1 = t0[b1ind,:][0]
 # plt.plot(x, zt, "k-")
 # plt.plot(x, zs, "r-")
 plt.plot(t0[0:nt0,0], t0[0:nt0,1], "bo")
 plt.plot(t0[nt0:,0], t0[nt0:,1], "ro")
-#plt.plot(b0[:,0], b0[:,1], "go")
+# plt.plot(b0[:,0], b0[:,1], "go")
+# plt.plot(b1[:,0], b1[:,1], "mo")
+
 plt.savefig("asplotgek.png")
+import pdb; pdb.set_trace()
 # plt.figure(1)
 # plt.clf()
 # plt.plot(x, zlv, "b-")

aerostruct_paper/surrogate/example_problems.py
@@ -261,7 +261,7 @@ class MultiDimJump(Problem):
         self.xlimits[:, 0] = -2.5
         self.xlimits[:, 1] = 2.5
 
-        self.t = np.random.normal(size=self.options["ndim"])
+        self.t = np.array([1.,1.])# np.random.normal(size=self.options["ndim"])
         self.t = self.t/np.linalg.norm(self.t)
         self.alpha = self.options["alpha"]
 
@@ -288,7 +288,7 @@ class MultiDimJump(Problem):
             if kx is None:
                 y[i,0] = np.arctan(self.alpha*work)
             else:
-                work2 = (1./(1.+work*work))
+                work2 = (1./(1.+work*work*self.alpha*self.alpha))*self.alpha
                 y[i,0] = work2*self.t[kx]
 
         return y

aerostruct_paper/surrogate/getxnew.py
@@ -1,4 +1,5 @@
 from optimizers import optimize
+import copy
 from defaults import DefaultOptOptions
 import numpy as np
 from smt.surrogate_models import GEKPLS
@@ -53,15 +54,17 @@ def adaptivesampling(func, model, rcrit, bounds, ntr, options=None):
 
     #TODO: Alternate Stopping Criteria
     count = int(ntr/rcrit.nnew)
+    hist = []
+    
     for i in range(count):
         t0 = model.training_points[None][0][0]
         f0 = model.training_points[None][0][1]
-        g0 = []
+        g0 = rcrit.grad
         nt, dim = t0.shape
         x0 = np.zeros([1, dim])
-        if(isinstance(model, GEKPLS) or isinstance(model, POUSurrogate)):
-            for i in range(dim):
-                g0.append(model.training_points[None][i+1][1])
+        # if(isinstance(model, GEKPLS) or isinstance(model, POUSurrogate)):
+        #     for i in range(dim):
+        #         g0.append(model.training_points[None][i+1][1])
 
         # get the new points
         xnew = np.array(getxnew(rcrit, x0, bounds, options))
@@ -69,16 +72,19 @@ def adaptivesampling(func, model, rcrit, bounds, ntr, options=None):
         # add the new points to the model
         t0 = np.append(t0, xnew, axis=0)
         f0 = np.append(f0, func(xnew), axis=0)
-        if(isinstance(model, GEKPLS) or isinstance(model, POUSurrogate)):
-            for i in range(dim):
-                g0[i] = np.append(g0[i], func(xnew, i), axis=0)
+        g0 = np.append(g0, np.zeros([xnew.shape[0], xnew.shape[1]]), axis=0)
+        for i in range(dim):
+            g0[nt:,i] = func(xnew, i)[:,0]
+
         model.set_training_values(t0, f0)
         if(isinstance(model, GEKPLS) or isinstance(model, POUSurrogate)):
             for i in range(dim):
                 model.set_training_derivatives(t0, g0[i], i)
         model.train()
 
-        # replace criteria
-        rcrit.initialize(model)
+        hist.append(copy.deepcopy(rcrit))
 
-    return model, rcrit
+        # replace criteria
+        rcrit.initialize(model, g0)
+
+    return model, rcrit, hist

aerostruct_paper/surrogate/refinecriteria.py
@@ -150,11 +150,14 @@ class looCV(ASCriteria):
 # Hessian estimation and direction criteria
 
 class HessianFit(ASCriteria):
-    def __init__(self, model, **kwargs):
+    def __init__(self, model, grad, **kwargs):
 
         self.bads = None
+        self.bad_eigs = None
+        self.bad_list = None
         self.bad_nbhd = None
         self.bad_dirs = None
+        self.grad = grad
 
         super().__init__(model, **kwargs)
 
@@ -173,12 +176,12 @@ class HessianFit(ASCriteria):
         #options: linear, quadratic
         self.options.declare("error", "linear", types=str)
 
-        self.options.declare("improve", 0.05, types=float)
+        self.options.declare("improve", 0, types=int)
 
         #number of closest points to evaluate nonlinearity measure
         self.options.declare("neval", self.dim*2, types=int)
         
-    def initialize(self, model=None):
+    def initialize(self, model=None, grad=None):
         
         if(model is not None):
             self.model = copy.deepcopy(model)
@@ -186,17 +189,20 @@ class HessianFit(ASCriteria):
             self.dim = self.model.training_points[None][kx][0].shape[1]
             self.ntr = self.model.training_points[None][kx][0].shape[0]
 
-        self.nnew = int(self.ntr*self.options["improve"])
+        if(grad is not None):
+            self.grad = grad
+
+        self.nnew = self.options["improve"]#int(self.ntr*self.options["improve"])
         if(self.nnew == 0):
             self.nnew = 1
-        
+
         trx = self.model.training_points[None][0][0]
         trf = self.model.training_points[None][0][1]
         trg = np.zeros_like(trx)
+        trg = self.grad
         if(isinstance(self.model, GEKPLS)):
             for j in range(self.dim):
                 trg[:,j] = self.model.training_points[None][j+1][1].flatten()
-
         dists = pdist(trx)
         dists = squareform(dists)
 
@@ -241,6 +247,8 @@ class HessianFit(ASCriteria):
                                                     trg[i,:], trg[indn[i][1:self.options["neval"]+1],:])
 
                     hess.append([evalm, evecm])
+
+
 
         if(self.options["hessian"] == "surrogate"):
 
@@ -293,6 +301,7 @@ class HessianFit(ASCriteria):
 
         # 3a. Take the highest eigenvalue/vector of each Hessian
         opt_dir = []
+        opt_val = []
         if(self.options["interp"] == "arnoldi"):
             for i in badlist:
                 opt_dir.append(hess[i][1])
@@ -302,8 +311,11 @@ class HessianFit(ASCriteria):
                 eigvals, eigvecs = eig(H)
                 o = np.argsort(abs(eigvals))
                 opt_dir.append(eigvecs[:,o[-1]])
+                opt_val.append(eigvals[o[-1]])
 
         # we have what we need
+        self.bad_list = badlist
+        self.bad_eigs = opt_val
         self.bads = bads
         self.bad_nbhd = bad_nbhd
         self.bad_dirs = opt_dir
@@ -340,19 +352,30 @@ class HessianFit(ASCriteria):
 
     def pre_asopt(self, bounds, dir=0):
         xc = self.bads[dir]
+        gc = self.grad[self.bad_list[dir],:]
+        eig = self.bad_eigs[dir]
         xdir = self.bad_dirs[dir]
         trx = self.model.training_points[None][0][0]
         nbhd = trx[self.bad_nbhd[dir],:]
         dists = pdist(np.append(np.array([xc]), nbhd, axis=0))
         dists = squareform(dists)
         B = max(dists[0,:])
-
         # check if we need to limit further based on bounds
         p0, p1 = boxIntersect(xc, xdir, bounds)
-        bp = min(B, p1)
-        bm = max(-B, p0)
+        bp = p1# min(B, p1)
+        bm = p0# max(-B, p0)
 
-        return np.array([0.+0.1*bp]), Bounds(bm, bp)
+        # choose the direction to go
+
+        # if concave up, move up the gradient, if concave down, move down the gradient
+        work = np.dot(gc, xdir)
+        adir = np.sign(work)*np.sign(eig)
+        if(adir > 0):
+            bm = 0
+        else:
+            bp = 0
+
+        return np.array([0.+adir*0.01*B]), Bounds(bm, bp)
 
     def post_asopt(self, x, dir=0):
 

aerostruct_paper/surrogate/utils.py
@@ -360,7 +360,7 @@ def boxIntersect(xc, xdir, bounds):
 # xdir = xdir/np.linalg.norm(xdir)
 
 # p0, p1 = boxIntersect(xc, xdir, xlimits)
-
+# import pdb; pdb.set_trace()
 
 
 # dim = 2

aerostruct_paper/test/test_example_problems.py
@@ -18,7 +18,7 @@ class ProblemDiffTest(unittest.TestCase):
         xg = np.random.rand(dim)*spread - offset
 
         fgm = trueFunc(np.array([xg-h*xi]))
-        fgp = trueFunc(np.array([xg-h*xi]))
+        fgp = trueFunc(np.array([xg+h*xi]))
         ga = np.zeros([1,dim])
         for i in range(dim):
             ga[0,i] = trueFunc(np.array([xg]), i)
@@ -26,6 +26,8 @@ class ProblemDiffTest(unittest.TestCase):
         finitediff = (1./(2*h))*(fgp-fgm)
         analytic = np.dot(ga, xi)
         err = abs(analytic - finitediff)
+        self.assertTrue(err < 1.e-8)
+
 
     def test_FuhgP8Gradient(self):
         h = 1e-5
@@ -37,7 +39,7 @@ class ProblemDiffTest(unittest.TestCase):
         xg = np.random.rand(dim)*spread - offset
 
         fgm = trueFunc(np.array([xg-h*xi]))
-        fgp = trueFunc(np.array([xg-h*xi]))
+        fgp = trueFunc(np.array([xg+h*xi]))
         ga = np.zeros([1,dim])
         for i in range(dim):
             ga[0,i] = trueFunc(np.array([xg]), i)
@@ -45,6 +47,8 @@ class ProblemDiffTest(unittest.TestCase):
         finitediff = (1./(2*h))*(fgp-fgm)
         analytic = np.dot(ga, xi)
         err = abs(analytic - finitediff)
+        self.assertTrue(err < 1.e-8)
+
 
     def test_QuadHadamardGradient(self):
         h = 1e-5
@@ -57,7 +61,7 @@ class ProblemDiffTest(unittest.TestCase):
         xg = np.random.rand(dim)*spread - offset
 
         fgm = trueFunc(np.array([xg-h*xi]))
-        fgp = trueFunc(np.array([xg-h*xi]))
+        fgp = trueFunc(np.array([xg+h*xi]))
         ga = np.zeros([1,dim])
         for i in range(dim):
             ga[0,i] = trueFunc(np.array([xg]), i)
@@ -66,6 +70,7 @@ class ProblemDiffTest(unittest.TestCase):
         analytic = np.dot(ga, xi)
         err = abs(analytic - finitediff)
 
+        self.assertTrue(err < 1.e-8)
 
 if __name__ == '__main__':
     unittest.main()

aerostruct_paper/test/test_utils.py
@@ -2,12 +2,26 @@ import unittest
 import numpy as np
 import sys
 sys.path.insert(1,"../")
+sys.path.insert(1,"../surrogate")
 
-from surrogate.utils import quadraticSolve, quadraticSolveHOnly, symMatfromVec, maxEigenEstimate
+from surrogate.utils import quadraticSolve, quadraticSolveHOnly, symMatfromVec, maxEigenEstimate, boxIntersect
+from smt.problems import RobotArm
 
-class ProblemDiffTest(unittest.TestCase):
+class UtilTest(unittest.TestCase):
     
-    None
+    def test_boxIntersect(self):
+        dim = 2
+        trueFunc = RobotArm(ndim=dim)
+        xlimits = trueFunc.xlimits
+
+        xc = np.array([0.9, np.pi])
+        xdir = np.array([1, 4])
+        xdir = xdir/np.linalg.norm(xdir)
+
+        p0, p1 = boxIntersect(xc, xdir, xlimits)
+
+        self.assertTrue(p0 - -3.238279585853798 < 1.e-14)
+        self.assertTrue(p1 - 0.41231056256176596 < 1.e-14)
 # dim = 2
 # trueFunc = Quad2D(ndim=dim, theta=np.pi/4)
 # xlimits = trueFunc.xlimits