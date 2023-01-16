9dc9ceddeaf404f65f2a70c688a079d9ae541e87
Garo Bedonian <garo.bedonian@gmail.com>
2023-01-16 14:40:37 -0500

    ready to compute shock test data

--- scratch/impinge_sweep_smt.py
+++ scratch/impinge_sweep_smt.py
@@ -1,21 +1,19 @@
 import sys, os
-sys.path.insert(1,"../surrogate")
-from sutils import divide_cases
-sys.path.insert(1,"../mphys")
-from impinge_analysis import Top
-from shock_problem import ImpingingShock
+from utils.sutils import divide_cases
+from functions.shock_problem import ImpingingShock
 from smt.sampling_methods import LHS
 from mpi4py import MPI
 import pickle
 import numpy as np
 
+import mphys_comp.impinge_setup as default_impinge_setup
 
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-Ncase = 70
+Ncase = 12
 
 inputs = ["shock_angle", "rsak"]
 dim = len(inputs)
@@ -23,18 +21,25 @@ xlimits = np.zeros([dim,2])
 xlimits[0,:] = [23., 27.]
 xlimits[1,:] = [0.36, 0.51]
 sampling = LHS(xlimits=xlimits, criterion='m')
-x = sampling(Ncase)
-x = comm.bcast(x, root=0)
-#if rank == 0:
-title = f'{Ncase}_shock_results'
-#    if not os.path.isdir(title):
-#        os.mkdir(title)
-# with open(f'./{title}/x.pickle', 'rb') as f:
-#     x = pickle.load(f)
 
-func = ImpingingShock(ndim=dim, input_bounds=xlimits, inputs=inputs)
+x = sampling(Ncase)
+title = f'{Ncase}_shock_results'
+if rank == 0:
+    if not os.path.isdir(title):
+        os.mkdir(title)
+    else:
+        with open(f'./{title}/x.pickle', 'rb') as f:
+            x = pickle.load(f)
+x = comm.bcast(x, root=0)
+
+problem_settings = default_impinge_setup
+problem_settings.aeroOptions['L2Convergence'] = 1e-15
+problem_settings.aeroOptions['printIterations'] = False
+problem_settings.aeroOptions['printTiming'] = False
+
+func = ImpingingShock(ndim=dim, input_bounds=xlimits, inputs=inputs, problem_settings=problem_settings)
 sta = 0
-sto = 120
+sto = 12
 y = func(x[sta:sto])
 
 g = np.zeros_like(x[sta:sto])
